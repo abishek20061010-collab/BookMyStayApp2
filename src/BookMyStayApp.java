305f281388ae4f45b0f4cad4acfb007ee227acb5
abishek20061010-collab <abishek20061010@gmail.com>
2026-04-01 15:16:44 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,21 @@
+/**
+ * Hotel Booking Application Entry Point
+ * This class represents the starting point of the Book My Stay App.
+ *
+ * It demonstrates how a Java application begins execution
+ * and displays a welcome message to the user.
+ *
+ * @author Abishek
+ * @version 1.0
+ */
+public class BookMyStayApp
+
+    /**
+     * Main method - Entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
